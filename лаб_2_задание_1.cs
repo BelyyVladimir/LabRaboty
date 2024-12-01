e81925009e589f2857e960c692128a9f6eb0ae29
BelyyVladimir <vladimrbelyy@yandex.ru>
2024-12-01 19:58:48 +0600

    Update and rename Program.cs to лаб_2_задание_1.cs

---
+++ лаб_2_задание_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class zdn
+{
+    static void Main()
+    {
+        int a = Convert.ToInt32 (Console.ReadLine()); // колво элементов
+        int j = 0;// колво эл меньше нуля в итоге
+        for (int i = 1,k; i <= a; i++)
+        { 
+            k = Convert.ToInt32( Console.ReadLine());
+            if (k < 0) { j++; }
+            
+        }
+        Console.WriteLine(j);
+    }
+}
