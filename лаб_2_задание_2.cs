47fcac6c89c911dfbba716d59ff9d540d052d044
BelyyVladimir <vladimrbelyy@yandex.ru>
2024-12-01 20:12:15 +0600

    Update лаб_2_задание_2.cs

--- лаб_2_задание_2.cs
+++ лаб_2_задание_2.cs
@@ -1,16 +1,21 @@
-﻿using System;
+using System;
 class zdn
 {
     static void Main()
     {
-        int a = Convert.ToInt32 (Console.ReadLine()); // колво элементов
+        int a = Convert.ToInt32(Console.ReadLine()); 
         int m1 = 0;
         int m2 = 0;
         for (int i = 1, k = 0; i <= a; i++)
-        { 
-            k = Convert.ToInt32( Console.ReadLine());
-            if (k >= m1) { m2 = m1; m1 = k;}
-            
+        {
+            k = Convert.ToInt32(Console.ReadLine());
+            if (i == 1) m1 = m2 = k;
+            else if (i == 2)
+            {
+                if (k > m1) m1 = k;
+                else m2 = k;
+            }
+            else if (k >= m1) { m2 = m1; m1 = k; } 
         }
         Console.WriteLine(m2);
     }
