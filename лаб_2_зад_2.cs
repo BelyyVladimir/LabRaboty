34197f57b729dc122e95a3721526587b3ec7be31
BelyyVladimir <vladimrbelyy@yandex.ru>
2024-12-01 20:00:48 +0600

    Update and rename Program.cs to лаб_2_зад_2.cs

---
+++ лаб_2_зад_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class zdn
+{
+    static void Main()
+    {
+        int a = Convert.ToInt32 (Console.ReadLine()); // колво элементов
+        int m1 = 0;
+        int m2 = 0;
+        for (int i = 1, k = 0; i <= a; i++)
+        { 
+            k = Convert.ToInt32( Console.ReadLine());
+            if (k >= m1) { m2 = m1; m1 = k;}
+            
+        }
+        Console.WriteLine(m2);
+    }
+}
