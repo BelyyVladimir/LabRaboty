f089a2d91a598b2ffe4a671a7947cf27bf3ab7ef
BelyyVladimir <vladimrbelyy@yandex.ru>
2024-12-01 20:11:17 +0600

    Update лаб_2_задание_3.cs

--- лаб_2_задание_3.cs
+++ лаб_2_задание_3.cs
@@ -3,31 +3,20 @@ class zdn
 {
     static void Main()
     {
-        int a = Convert.ToInt32 (Console.ReadLine()); 
+        int a = Convert.ToInt32(Console.ReadLine()); 
         int s = 0;
-        for (int i = 1, k = 0,cl = 0,c = 0,cr = 0; i <= a; i++)
-        { 
-            k = Convert.ToInt32( Console.ReadLine());
-            if (i == 1)
-            { 
-                cl = c = cr = k;
-            }
-            else if ( i == 2) 
-            {
-                cl = c = cr;
-                cr = k;
-            }
+        for (int i = 1, k = 0, cl = 0, c = 0, cr = 0; i <= a; i++)
+        {
+            k = Convert.ToInt32(Console.ReadLine());
+            if (i == 1) c = k; 
+            else if (i == 2) cr = k;
             else
             {
                 cl = c;
                 c = cr;
                 cr = k;
-                if ((cl > c) && (c < cr))
-                {
-                    s++;
-                }
+                if ((cl > c) && (c < cr)) s++;
             }
-            
         }
         Console.WriteLine(s);
     }
